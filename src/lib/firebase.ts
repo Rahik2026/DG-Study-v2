e824d7dea1f23840cd248b425698651f32e26804
Rahik2026 <rahik.islam2000@gmail.com>
2026-05-26 11:48:10 +0600

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -15,3 +15,4 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
+console.log("Firebase Connected Successfully");
